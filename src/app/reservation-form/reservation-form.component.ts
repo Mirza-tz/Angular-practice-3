02d3e4df62827b5f23dc4c719032be4a176cdf40
mirza <mirza.hamzabegovic91@gmail.com>
2025-03-16 14:20:32 +0100

    last commit

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -37,24 +37,30 @@ constructor(
    })
 
    let id = this.activatedRoute.snapshot.paramMap.get('id')
+
    if(id) {
-   let reservation = this.reservationService.getReservation(id)
+
+  this.reservationService.getReservation(id).subscribe(reservation =>{
    if(reservation)
    this.reservationForm.patchValue(reservation)
-   }
-  }
+   })
+  }}
 
 onSubmit() {
   if(this.reservationForm.valid) {
    let reservation: Reservation = this.reservationForm.value;
 
     let id = this.activatedRoute.snapshot.paramMap.get('id')
+
+
     if (id) {
 
-     
-      this.reservationService.updateReservation(id,reservation)
+      this.reservationService.updateReservation(id,reservation).subscribe(()=>{
+        console.log("Update request processed")
+      })
     } else {
-      this.reservationService.addReservation(reservation)
+      this.reservationService.addReservation(reservation).subscribe(()=>
+      {console.log("Add request processed")})
     }
 
    this.router.navigate(['/list'])
